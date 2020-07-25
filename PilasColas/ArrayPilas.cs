cb49464eec294b46a85deed51d090547ff4b0f1a
Antonio Rogel <arogel@unikino.edu.mx>
2020-07-25 13:44:02 -0700

    Agregando clase ArrayPilas

---
+++ PilasColas/ArrayPilas.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PilasColas
+{
+    public class ArrayPilas
+    {
+        private string[] array;
+        private int max;
+        private int tope;
+
+        public ArrayPilas(int tamanio)
+        {
+            this.array = new string[tamanio];
+            this.tope = 0;
+            this.max = array.Length - 1;
+        }
+
+        private bool ValidaVacio()
+        {
+            return (tope < 1);
+        }
+
+        private bool ValidaLleno()
+        {
+            return (tope > max);
+        }
+
+        public void Agregar(string dato)
+        {
+            if (ValidaLleno())
+            {
+                throw new Exception("arreglo lleno");
+            }
+
+            array[tope] = dato;
+            tope++;
+        }
+
+        public void Eliminar()
+        {
+            if (ValidaVacio())
+            {
+                throw new Exception("arreglo vacío");
+            }
+
+            tope--;
+            array[tope] = null;
+        }
+
+        public string Imprimir()
+        {
+            string dato = string.Empty;
+
+            if (ValidaVacio())
+            {
+                return "arreglo vacío";
+            }
+
+            for (int i = 0; i < tope; i++)
+            {
+                if (i > 0)
+                {
+                    dato += "\n";
+                }
+
+                dato += $"[{i}] - {array[i]}";
+            }
+
+            return dato;
+        }
+    }
+}
--- PilasColas/Program.cs
+++ PilasColas/Program.cs
@@ -6,7 +6,60 @@ namespace PilasColas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            try
+            {
+                ArrayPilas pila = new ArrayPilas(5);
+
+                pila.Agregar("Cero");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("Uno");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("Dos");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("Tres");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("Cuatro");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("Cinco");
+                Console.WriteLine(pila.Imprimir());
+
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        
         }
     }
 }
